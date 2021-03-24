c5eb21b35924651a4b629b9a2122c7f8ec584651
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-03-24 19:32:07 +0530

    Rename package

--- pyportfolio/transaction_filters.py
+++ pyportfolio/transaction_filters.py
@@ -1,5 +1,5 @@
 def get_transaction_type_filter(transaction_type):
-        return lambda transaction: transaction["type"] == transaction_type
+    return lambda transaction: transaction["type"] == transaction_type
     
 def get_transaction_older_than_filter(days):
     return lambda transaction: transaction["Days"] > days
