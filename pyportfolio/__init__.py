9b818b84926629de82becc1024fe7a51bc4340bc
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2021-06-27 22:09:27 +0530

    Fix #2: Error handling for casparser errors (#12)
    * Capture Incorrext password and other casparser exceptions
    
    * Bump up version

--- pyportfolio/__init__.py
+++ pyportfolio/__init__.py
@@ -1,7 +1,7 @@
 from .models import Scheme
 from .portfolio import Portfolio
 from .scheme_factory import create_scheme_from_casparser
+from .__version__ import __version__
 
-__version__ = "1.0.0"
 __author__ = "Kaushik S Kalmady / kaushiksk"
 __license__ = "MIT"
