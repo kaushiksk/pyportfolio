0655816de95fddd632bb1dfea11911218877480e
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-27 13:10:43 +0530

    Use closures with strong typing for filters

--- pyportfolio/transaction_filters.py
+++ pyportfolio/transaction_filters.py
@@ -1,6 +1,32 @@
-def get_transaction_type_filter(transaction_type):
-    return lambda transaction: transaction["type"] == transaction_type
+from typing import Callable
+from casparser.enums import TransactionType
+
+from .models import Transaction
+from .utils import or_filter
+
+TransactionFilterType = Callable[[Transaction], bool]
 
 
-def get_transaction_older_than_filter(days):
-    return lambda transaction: transaction["days"] > days
+def get_transaction_type_filter(transaction_type: TransactionType) -> TransactionFilterType:
+    def transaction_type_filter(transaction: Transaction):
+        return transaction.type == transaction_type
+
+    return transaction_type_filter
+
+
+def get_transaction_older_than_filter(days: int) -> TransactionFilterType:
+    def transaction_older_than_filter(transaction: Transaction):
+        return transaction.days > days
+
+    return transaction_older_than_filter
+
+
+# Filters
+purchase_transaction_filter = or_filter(
+    [
+        get_transaction_type_filter(TransactionType.PURCHASE),
+        get_transaction_type_filter(TransactionType.PURCHASE_SIP),
+    ]
+)
+redemption_transaction_filter = get_transaction_type_filter(TransactionType.REDEMPTION)
+stampduty_transaction_filter = get_transaction_type_filter(TransactionType.MISC)
